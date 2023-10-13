a542e263d527a62397db9624a8e7a5ee1739c359
Francesco Gobello <francescogobello1@gmail.com>
2023-10-13 16:50:10 +0200

    card recensioni compleatate

--- main.js
+++ main.js
@@ -10,6 +10,7 @@ let imgNum=document.querySelector('#imgNum')
 let fnum=document.querySelector('#fnum')
 let snum=document.querySelector('#snum')
 let tnum=document.querySelector('#tnum')
+let swiperWrapper=document.querySelector('.swiper-wrapper')
 
 
 navbar.style.height="170px";
@@ -17,14 +18,14 @@ navbar.classList.remove("bverde")
 navbar.classList.add("bblack")
 titolo.classList.add('black')
 link.forEach((el)=>{
-el.style.color='#d8dbe2'
-el.addEventListener(("mouseenter"),()=>{
-    el.style.color='#8cc5c2'
-})
-el.addEventListener(("mouseleave"),()=>{
     el.style.color='#d8dbe2'
-    
-})
+    el.addEventListener(("mouseenter"),()=>{
+        el.style.color='#8cc5c2'
+    })
+    el.addEventListener(("mouseleave"),()=>{
+        el.style.color='#d8dbe2'
+        
+    })
 })
 logo.src="http://127.0.0.1:5500/media/logobianco.png"
 imgSfondo.src="http://127.0.0.1:5500/media/img-sfondo-scura.png"
@@ -54,8 +55,8 @@ window.addEventListener(("scroll"), ()=>{
         bordo.classList.remove('bordon')
         bordo.classList.add('bordov')
         imgNum.src="http://127.0.0.1:5500/media/bwink_tsp_06_single_02.png"
-
-
+        
+        
     } else {
         navbar.style.height="170px";
         navbar.classList.remove("bverde");
@@ -69,9 +70,9 @@ window.addEventListener(("scroll"), ()=>{
             })
             el.addEventListener(("mouseleave"),()=>{
                 el.style.color='#d8dbe2'
-            
-
-        })
+                
+                
+            })
         });
         logo.src="http://127.0.0.1:5500/media/logobianco.png";
         imgSfondo.src="http://127.0.0.1:5500/media/img-sfondo-scura.png"
@@ -96,27 +97,135 @@ function incremento(valore, incremento, numIncrementare, intervallo) {
         }
     },intervallo)
 }
-// observer.observe(tnum)
 
-// let observer=new IntersectionObserver ( (entries)=>{
-//     entries.forEach((el)=>{
-//         if (el.isIntersecting) {
-            // incremento(94, 1, fnum, 20);
-            // incremento(8000, 100, snum, 25);
-            // incremento(873, 10, tnum, 22.5);
+let observer=new IntersectionObserver ( (entries)=>{
+    entries.forEach((el)=>{
+        if (el.isIntersecting) {
+            incremento(94, 1, fnum, 40);
+            incremento(8000, 100, snum, 50);
+            incremento(873, 10, tnum, 45);
             
-//             observer.disconnect()
-//         }
-//     })
-// })
+            observer.disconnect()
+        }
+    })
+})
 
-// let observer= new IntersectionObserver ((entries)=>{
-//     entries.forEach(element => {
+observer.observe(tnum)
+
+
+let articoli=[
+    {nome:"chitarra", categoria:"Musica", prezzo:'60' },
+    {nome:"palla", categoria:"Giochi", prezzo:'10' },
+    {nome:"spugna", categoria:"Pulizie", prezzo:'1,50' },
+    {nome:"Arpa", categoria:"Musica", prezzo:'80' },
+    {nome:"Risiko", categoria:"Giochi", prezzo:'45' },
+    {nome:"Gamba di legno", categoria:"Garpez", prezzo:'60.000' },
+    {nome:"Joystick", categoria:"Giochi", prezzo:'70' },
+]
+
+articoli.forEach((el,i)=>{
+    if (i>=(articoli.length -3)) {
         
-//     });
-// })
+        let div=document.createElement('div')
+        div.classList.add("col-12", 'col-md-3', 'cardAnnunci',  'p-2', 'b-card', "w3-hover-shadow", 'mx-1', 'mt-2')
+        div.innerHTML=`
+        <img class="card-img-top imgAnnunci shadow " src="http://picsum.photos/${200+i}" alt="">
+        <p class="text-center fs-4">${el.nome}</p>
+        <p class="text-center opacity-50">${el.categoria}</p>
+        <p class="text-center fs-2">${el.prezzo} €</p>
+        </div>
+        `
+        rowAnnunci.appendChild(div)
+    }
+})
 
-            incremento(94, 1, fnum, 20);
-            incremento(8000, 100, snum, 25);
-            incremento(873, 10, tnum, 22.5);
-    
+
+let reviews = [ 
+
+    {name: 'David', description: 'Bellissimo e sublime', vote: 5},
+    {name: 'Luca', description: "Loris l'avrebbe fatto meglio", vote: 3},
+    {name: 'Loris', description: 'Rivoglio i miei soldi indietro', vote: 1},
+    {name: 'Francesco', description: 'sciapo', vote: 4},
+    {name: 'Vercingetorige', description: "L'impero romano fa schifo e ci penso spesso", vote: 2 }
+
+]
+
+
+
+
+reviews.forEach( (review)=>{
+    let div = document.createElement('div');
+    div.classList.add('swiper-slide', 'p-2',  'd-flex', 'flex-column', 'justify-content-around' );
+    div.innerHTML= `
+    <p class="text-center fs-3 mt-2">${review.name}</p>
+                    <p class="text-center w3-medium ">${review.description}</p>
+                    <div class= "d-flex justify-content-center align-bottom mt-2>
+            ${createStar(review.vote)}
+        </div>
+        
+                </div>
+    `
+    swiperWrapper.appendChild(div)
+} )
+
+
+// FUNZIONE CREA STELLE
+function createStar(fullStars) {
+    let final = '';
+
+
+
+// funzione crea stelle piene
+        //ricordarsi che fullStars equivale al voto, quindi se l'utente mette 0 al voto, il primo ciclo non se lo guarda proprio perché la i parte da 0 e quindi 0 è minore di 0? no! ergo va al secondo ciclo che mi genera le stelle vuote
+    for(let i = 0; i <= fullStars; i ++){
+    final = final + `<i class="fa-solid fa-star"></i>`;
+    }
+
+
+// funzione crea stelle vuote
+        // 5 stelle totali, meno le stelle piene che ho già trovato su, ad  esempio 5 - 2 stelle piene, mi avanzano 3 stelle vuote, è lì che questo nuovo ciclo si deve fermare e mi deve generare le restanti stelle vacanti
+        for(let i = 0; i < 5 - fullStars; i++ ){
+            final = final + `<i class="fa-regular fa-star"></i>`
+        }
+    // mi devo ricordare di ritornare il valore di final, altrimenti avrò in console un undefined
+    return final;
+}
+
+//L'INVOCAZIONE E' NELL INNERHTML DI REVIEWS.FOREACH
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+var swiper = new Swiper(".mySwiper", {
+    effect: "cards",
+    grabCursor: true,
+    loop: true,
+});
